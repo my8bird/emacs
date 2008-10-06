28453db19d93df7685da55d2de1a57e1b9bf5af4
unknown <nlandis@.ISLLCNETS.Studentloan.org>
2008-10-06 08:41:05 -0500

    init

---
+++ mine/customizations.el
@@ -0,0 +1,28 @@
+;; Key Bindings
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-x\C-j" 'speedbar)
+(global-set-key "\r" 'newline-and-indent)
+
+(global-unset-key "\C-z")
+
+;; Setup Environmental Variables
+(setq default-major-mode 'text-mode)
+(setq inhibit-startup-message t)
+(setq custom-file (concat emacs-root "/mine/customizations.el"))
+
+;; Auto revert files
+(global-auto-revert-mode 1)
+
+;; Don't make backups
+(setq make-backup-files nil)
+(setq version-control nil)
+
+;; Look Pretty
+(global-hl-line-mode 1)
+
+;; Miscallaneous Things
+(mouse-wheel-mode t)
+(setq visible-bell t)
+;; (setq-default show-trailing-whitespace t)
+(put 'downcase-region 'disabled nil)
---
+++ mine/dependencies.el
@@ -0,0 +1,54 @@
+;; eeio
+(add-path "site-lisp/eieio")
+(require 'eieio)
+
+;; Semantic
+(add-path "site-lisp/semantic/")
+(setq semantic-load-turn-everything-on t)
+(require 'semantic-load)
+
+;; speedbar
+(add-path "site-lisp/speedbar/")
+(require 'speedbar)
+
+;; Git Integration
+(add-path "site-lisp/git/")
+(require 'git)
+(require 'vc-git)
+(require 'git-blame)
+(add-to-list 'vc-handled-backends 'GIT)
+
+;; Color theme
+(add-path "site-lisp/color-theme-6.6.0")
+(require 'color-theme)
+(color-theme-initialize)
+(load-file (concat emacs-root "el/color-theme-twilight.el"))
+(color-theme-twilight)
+
+;; yasnippet
+(add-path "site-lisp/yasnippet-0.5.4/")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory (concat emacs-root "site-lisp/yasnippet-0.5.4/snippets/"))
+
+;; nxml-mode
+(load-file (concat emacs-root "site-lisp/nxml-mode-20041004/rng-auto.el"))
+(setq auto-mode-alist
+      (cons '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
+	    auto-mode-alist))
+
+;; scala
+(add-path "site-lisp/scala")
+(require 'scala-mode-auto)
+
+;; Misc Files
+(add-path "el/")
+(require 'find-recursive)
+(require 'tidy)
+(require 'tail)
+(require 'keywiz)
+(require 'unit-test)
+
+;; ido-mode
+(require 'ido)
+(ido-mode t)
