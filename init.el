d50fba292a73d446b4a538186043b07964c41350
Nathan Landis <nathan@nathan-desktop.(none)>
2009-05-05 14:04:30 -0500

    updated init.el

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;;;; Nate's .emacs file
 
 (defvar emacs-root (concat (getenv "HOME") "/.emacs.d/"))
@@ -14,8 +15,28 @@
 (load "ecb.el")
 (load "erlang.el")
 (load "javascript")
+(load "sass-mode.el")
+(load "haml-mode.el")
+(autoload 'python-mode "python-mode" "Python editing mode." t)
+(setq-default py-indent-offset 3)
+(setq-default indent-tabs-mode nil)
 (load "nxhtml/autostart.el")
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
+
+(setq scroll-bar-mode-explicit t)
+(set-scroll-bar-mode `right)
+
+(setq-default show-trailing-whitespace t)
+
+(tool-bar-mode nil)
+
+(global-set-key (kbd "C-c b") 'w3m-bookmark-view)
+(global-set-key (kbd "C-c w") 'delete-trailing-whitespace)
+
+(fset 'go-to-qt-api
+   [?\M-x ?w ?3 ?m ?- ?b ?r ?o tab return ?h ?t ?t ?p ?: ?/ ?/ ?d ?o ?c ?. ?t ?r ?o ?l ?l ?t ?e ?c ?h ?. ?c ?o ?m ?/ ?4 ?. ?5 ?/ ?c ?a ?l backspace backspace ?l ?a ?s ?s ?e ?s ?. ?h ?t ?m ?l return])
+
+(global-set-key (kbd "C-c q") 'go-to-qt-api)
 
 (message "My .emacs loaded in %ds." (destructuring-bind (hi lo ms) (current-time) (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
