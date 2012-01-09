4b76069835717f0068372b211ad89bdfc335beaa
nathan landis <my8bird@gmail.com>
2012-01-09 11:35:14 -0600

    remove some cruft and move to a better javascript module

--- init.el
+++ init.el
@@ -15,14 +15,16 @@
 (load "ruby.el")
 (load "ecb.el")
 (load "erlang.el")
-(load "javascript")
+;;(load "javascript")
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (load "sass-mode.el")
 (load "haml-mode.el")
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 (setq-default py-indent-offset 3)
 (setq-default indent-tabs-mode nil)
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
+;;(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
 
 (setq scroll-bar-mode-explicit t)
 (set-scroll-bar-mode `right)
@@ -35,15 +37,13 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/mine/ac-dict")
 (ac-config-default)
 
-(global-set-key (kbd "C-c b") 'w3m-bookmark-view)
 (global-set-key (kbd "C-c w") 'delete-trailing-whitespace)
-
-(fset 'go-to-qt-api
-   [?\M-x ?w ?3 ?m ?- ?b ?r ?o tab return ?h ?t ?t ?p ?: ?/ ?/ ?d ?o ?c ?. ?t ?r ?o ?l ?l ?t ?e ?c ?h ?. ?c ?o ?m ?/ ?4 ?. ?5 ?/ ?c ?a ?l backspace backspace ?l ?a ?s ?s ?e ?s ?. ?h ?t ?m ?l return])
-
-(global-set-key (kbd "C-c q") 'go-to-qt-api)
 
 ;; Me likey column count
 (column-number-mode t)
+
+(setq-default indent-tabs-mode nil) ; always replace tabs with spaces
+(setq-default tab-width 3) ; set tab width to 3 for all buffers
+(setq-default c-basic-offset 3)
 
 (message "My .emacs loaded in %ds." (destructuring-bind (hi lo ms) (current-time) (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
