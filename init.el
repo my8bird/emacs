df7ba362e46e6484bbbfee98f8c0e1c7eaac950a
nathan landis <my8bird@gmail.com>
2012-07-23 14:17:49 -0500

    Updates

--- init.el
+++ init.el
@@ -41,6 +41,38 @@
 (setq-default show-trailing-whitespace t)
 
 (tool-bar-mode nil)
+(setq imenu-auto-rescan 1)
+
+
+(defun flymake-create-temp-intemp (file-name prefix)
+  (unless (stringp file-name)
+    (error "Invalid file-name"))
+  (or prefix
+      (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext))
+         )
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
+
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-intemp))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
 
 ;; Syntax highlighting in python files
 (when (load "flymake" t)
