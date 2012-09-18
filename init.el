0822262b6baa6bec18adda85994b51db2388384a
Nathan Landis <my8bird@gmail.com>
2012-09-18 09:07:34 -0500

    Add color theme and select one that I like

--- init.el
+++ init.el
@@ -5,6 +5,14 @@
 
 (defvar *emacs-load-start* (current-time))
 
+;; Setup theming
+(add-path "color-theme")
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-charcoal-black)))
+
 
 ;; Add the packages I care about
 ;; -JavaScript
@@ -22,11 +30,6 @@
 (require 'scss-mode)
 (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
 
-;; Easier File Finder (C-x C-f)
-(require 'ido)
-(ido-mode t)
-
-
 ;; Set the whitespace runs I must follow
 (setq-default py-indent-offset 3)
 (setq-default indent-tabs-mode nil)
@@ -35,16 +38,16 @@
 (setq-default tab-width 3) ; set tab width to 3 for all buffers
 (setq-default c-basic-offset 3)
 
+;; - Tell me when I make stupid whitespaces
+(setq-default show-trailing-whitespace t)
+;; - Provide a simple way to fix white space
+(global-set-key (kbd "C-c w") 'delete-trailing-whitespace)
+
 
 ;; Setup the UI the way i like it
 ;; - move the scrollbars
 (setq scroll-bar-mode-explicit t)
 (set-scroll-bar-mode `right)
-
-;; - Tell me when I make stupid whitespaces
-(setq-default show-trailing-whitespace t)
-;; - Provide a simple way to fix white space
-(global-set-key (kbd "C-c w") 'delete-trailing-whitespace)
 
 ;; - There is no reason for the toolbar
 (tool-bar-mode nil)
@@ -55,11 +58,14 @@
 ;; - Show the column count in the mode line
 (column-number-mode t)
 
-
 ;; Key Bindings
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\r" 'newline-and-indent)
+
+;; Easier File Finder (C-x C-f)
+(require 'ido)
+(ido-mode t)
 
 ;; Auto revert files
 (global-auto-revert-mode 1)
